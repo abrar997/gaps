901f6ba63d6aef8615d76714341594e675ee910b
abrar997 <abraralrawi997@gmail.com>
2021-07-08 18:05:32 +0300

    not complete project

---
+++ js/الدخول.js
@@ -0,0 +1,89 @@
+
+
+
+
+function light(){
+var x=document.getElementById("lampa");
+document.body.classList.toggle("color")
+
+
+}
+
+
+function more() {
+    var x = document.getElementById("text");
+    var y = document.getElementById("card");
+    var z = document.getElementById("btn");
+
+    if (x.style.display ="none") {
+        x.style.display = "block"
+        y.style.backgroundColor =" #ad914a";
+        y.style.color = "white";
+        z.style.display = "none"
+    } else {
+        x.style.display = "none"
+    }
+}
+
+
+
+function more1() {
+    var x = document.getElementById("text1");
+    var y = document.getElementById("card1");
+    var z = document.getElementById("btn1");
+
+    if (x.style.display = "none") {
+        x.style.display = "block"
+        y.style.backgroundColor = " #ad914a";
+        y.style.color = "white";
+        z.style.display = "none"
+    } else {
+        x.style.display = "none"
+    }
+}
+
+
+function more2() {
+    var x = document.getElementById("text2");
+    var y = document.getElementById("card2");
+    var z = document.getElementById("btn2");
+
+    if (x.style.display = "none") {
+        x.style.display = "block"
+        y.style.backgroundColor = " #ad914a";
+        y.style.color = "white";
+        z.style.display = "none"
+    } else {
+        x.style.display = "none"
+    }
+}
+
+
+
+function more3() {
+    var x = document.getElementById("text3");
+    var y = document.getElementById("card3");
+    var z = document.getElementById("btn3");
+
+    if (x.style.display = "none") {
+        x.style.display = "block"
+        y.style.backgroundColor = " #ad914a";
+        y.style.color = "white";
+        z.style.display = "none"
+    } else {
+        x.style.display = "none"
+    }
+}
+
+
+
+// function cloes(){
+//    var x=document.getElementById("card1");
+//     if (x.style.display = "block") {
+//         x.style.display = "none"
+   
+//     } else {
+//         x.style.display = "block"
+//     }
+// }
+
